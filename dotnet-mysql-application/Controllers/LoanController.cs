bde1ecf96012ef0edd8eee5ddeadbceb02eea952
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-05-21 14:42:18 +0300

    Implement CRUD for loans

--- dotnet-mysql-application/Controllers/LoanController.cs
+++ dotnet-mysql-application/Controllers/LoanController.cs
@@ -17,21 +17,73 @@ namespace dotnet_mysql_application.Controllers
         }
 
         [HttpGet("{id}")]
-        public IActionResult Loan(string id)
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public IActionResult GetLoanById(string id)
         {
-            var loan = db.Loans.Single(l => l.Id.ToString() == id);
+            var loan = db.Loans.Single(e => e.Id.ToString() == id);
             if (loan == null)
                 return NotFound();
             return new OkObjectResult(loan);
         }
 
         [HttpGet]
-        public IActionResult Loan()
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public IActionResult GetAllLoans()
         {
             var loans = db.Loans.ToList();
             if (loans == null)
                 return NotFound();
             return new OkObjectResult(loans);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(Loan))]
+        [ProducesResponseType(400)]
+        public IActionResult AddLoan([FromBody] Loan loan)
+        {
+            if (ModelState.IsValid) {
+                db.Loans.Add(loan);
+                db.SaveChanges();
+                return CreatedAtAction(nameof(GetLoanById), new { id = loan.Id },  loan);
+            }
+            else
+                return BadRequest();
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)]
+        public IActionResult UpdateLoan(string id, [FromBody] Loan loan)
+        {
+            var targetLoan = db.Loans.SingleOrDefault(e => e.Id.ToString() == id);
+            if (targetLoan == null)
+                return NotFound();
+            else if(ModelState.IsValid) {
+                targetLoan.EquipmentId = loan.Id;
+                targetLoan.Equipment = loan.Equipment;
+                targetLoan.UserId = loan.Id;
+                targetLoan.User = loan.User;
+                db.Loans.Update(targetLoan);
+                db.SaveChanges();
+                return Ok(loan);
+            }
+            else
+                return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(400)]
+        public IActionResult DeleteLoan(string id)
+        {
+            var loan = db.Loans.SingleOrDefault(e => e.Id.ToString() == id);
+            if (loan == null)
+                return NotFound();
+            db.Remove(loan);
+            db.SaveChanges();
+            return new NoContentResult();
         }
     }
 }
