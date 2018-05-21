fed1fd1a05296ded22a8e3878d6017d757ee0611
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-05-21 15:19:48 +0300

    Initial scaffolding for database seeding

--- dotnet-mysql-application/Helpers/InitDb.cs
+++ dotnet-mysql-application/Helpers/InitDb.cs
@@ -23,6 +23,20 @@ namespace dotnet_mysql_application.Helpers
                 {
                     Console.Write("An error has occured while initializing database: " + ex);
                 }
+
+                try
+                {
+                    var env = services.GetService<IHostingEnvironment>();
+                    if(env.IsDevelopment()) {
+                        Console.WriteLine("We are in dev, init DB here");
+                    } else {
+                        Console.WriteLine("We are in production, just seed the db");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Write("An error has occured while initializing database: " + ex);
+                }
             }
         }
     }
