32dba1e5aa0f863dd6b9a8f9506219cc0d2ae6dd
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-05-25 13:30:42 +0300

    Refactor frontend structure

--- dotnet-mysql-application/ClientApp/router/routes.tsx
+++ dotnet-mysql-application/ClientApp/router/routes.tsx
@@ -1,14 +1,7 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-import { Loans } from './components/Loans';
-import { Equipments } from './components/Equipments';
-import { Users } from './components/Users';
-import { Register } from './components/Register';
-import { Login } from './components/Login';
+import { Layout } from '../layout';
+import { Home, FetchData, Counter, Loans, Equipments, Users, Register, Login } from '../pages'
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
