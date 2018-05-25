bdf1d9c9aa6541e0133c79bd2f40eeb4cad01d03
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-05-25 11:28:45 +0300

    Lock backend behind auth

--- dotnet-mysql-application/Controllers/LoanController.cs
+++ dotnet-mysql-application/Controllers/LoanController.cs
@@ -5,7 +5,6 @@ using dotnet_mysql_application.Models;
 
 namespace dotnet_mysql_application.Controllers
 {
-    [Authorize(Policy = "ApiUser")]
     [Route("api/v1/[controller]")]
     [Produces("application/json")]
     public class LoanController : Controller
