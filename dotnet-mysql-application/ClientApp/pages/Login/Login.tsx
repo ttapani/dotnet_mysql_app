8fd34c0f7ae3a010ece0e41836eea09325e4f877
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-05-30 10:38:29 +0300

    Put login form inside a container

--- dotnet-mysql-application/ClientApp/pages/Login/Login.tsx
+++ dotnet-mysql-application/ClientApp/pages/Login/Login.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-import styles from './login.css';
+import * as styles from './login.css';
 
 export class Login extends React.Component<RouteComponentProps<{}>, {}> {
     constructor() {
@@ -27,15 +27,17 @@ export class Login extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
         return <div className="col-md-6 col-md-offset-3">
         <h2>Login</h2>
-            <form name="loginForm" onSubmit={this.handleSubmit} className={styles.signinorm}>
-                <div className={styles.signincontainer}>
+        <div className={styles.signincontainer}>
+            <form name="loginForm" onSubmit={this.handleSubmit} className={styles.signinform}>
+                
                     <label htmlFor="username">Enter email</label>
                     <input id="username" name="username" type="text" required/>
                     <label htmlFor="password">Enter password</label>
                     <input id="password" name="password" type="password" required/>
                     <button className={styles.signinbutton}>Login!</button>
-                </div>
+               
             </form>
+            </div>
         </div>;
     }
 }
