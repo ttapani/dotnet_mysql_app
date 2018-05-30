478b2973cccc2a9d8bf189b2e99ca8b0bc12ac7c
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-05-30 10:41:06 +0300

    Refactor frontend layout

--- dotnet-mysql-application/ClientApp/layout/Layout.tsx
+++ dotnet-mysql-application/ClientApp/layout/Layout.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { NavMenu } from './NavMenu';
+import { NavMenu } from '../navMenu';
 
 export interface LayoutProps {
     children?: React.ReactNode;
@@ -7,14 +7,10 @@ export interface LayoutProps {
 
 export class Layout extends React.Component<LayoutProps, {}> {
     public render() {
-        return <div className='container-fluid'>
-            <div className='row'>
-                <div className='col-sm-3'>
-                    <NavMenu />
-                </div>
-                <div className='col-sm-9'>
-                    { this.props.children }
-                </div>
+        return <div>
+            <NavMenu />
+            <div className='container-fluid'>
+                { this.props.children }
             </div>
         </div>;
     }
--- dotnet-mysql-application/ClientApp/navMenu/navMenu.tsx
+++ dotnet-mysql-application/ClientApp/navMenu/navMenu.tsx
@@ -1,20 +1,26 @@
 import * as React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import * as styles from './navMenu.css';
 
 export class NavMenu extends React.Component<{}, {}> {
+    constructor() {
+        super();
+    }
+
     public render() {
-        return <div className='main-nav'>
-                <div className='navbar navbar-inverse'>
-                <div className='navbar-header'>
-                    <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
-                        <span className='sr-only'>Toggle navigation</span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
-                    </button>
-                    <Link className='navbar-brand' to={ '/' }>LoanSystem</Link>
-                </div>
-                <div className='clearfix'></div>
+        return <div className={styles.mainNav}>
+                <div className={`navbar-default navbar navbar-inverse navbar-fixed-top`}>
+                    <div className='container-fluid'>
+                        <div className='navbar-header'>
+                            <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
+                                <span className='sr-only'>Toggle navigation</span>
+                                <span className='icon-bar'></span>
+                                <span className='icon-bar'></span>
+                                <span className='icon-bar'></span>
+                            </button>
+                            <Link className='navbar-brand' to={ '/' }>react</Link>
+                        </div>
+                {/* <div className='clearfix'></div> */}
                 <div className='navbar-collapse collapse'>
                     <ul className='nav navbar-nav'>
                         <li>
@@ -22,7 +28,7 @@ export class NavMenu extends React.Component<{}, {}> {
                                 <span className='glyphicon glyphicon-home'></span> Home
                             </NavLink>
                         </li>
-                        <li>
+                        {/* <li>
                             <NavLink to={ '/counter' } activeClassName='active'>
                                 <span className='glyphicon glyphicon-education'></span> Counter
                             </NavLink>
@@ -31,8 +37,8 @@ export class NavMenu extends React.Component<{}, {}> {
                             <NavLink to={ '/fetchdata' } activeClassName='active'>
                                 <span className='glyphicon glyphicon-th-list'></span> Fetch data
                             </NavLink>
-                        </li>
-                        <li>
+                        </li> */}
+                        {/* <li>
                             <NavLink to={ '/equipments' } activeClassName='active'>
                                 <span className='glyphicon glyphicon-th-list'></span> Equipments
                             </NavLink>
@@ -46,18 +52,22 @@ export class NavMenu extends React.Component<{}, {}> {
                             <NavLink to={ '/users' } activeClassName='active'>
                                 <span className='glyphicon glyphicon-th-list'></span> Users
                             </NavLink>
-                        </li>
-                        <li>
+                        </li> */}
+                        {/* <li>
                             <NavLink to={ '/register' } activeClassName='active'>
                                 <span className='glyphicon glyphicon-flash'></span> Register
                             </NavLink>
-                        </li>
+                        </li> */}
+                        
+                    </ul>
+                    <ul className='nav navbar-nav navbar-right'>
                         <li>
                             <NavLink to={ '/login' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-flash'></span> Login
+                                <span className='glyphicon glyphicon-log-in'></span> Login
                             </NavLink>
                         </li>
                     </ul>
+                </div>
                 </div>
             </div>
         </div>;
