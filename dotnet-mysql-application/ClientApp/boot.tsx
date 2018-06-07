85bc8f598841d9ff2b0567eb4f2ae414cfa95770
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-07 13:32:03 +0300

    Add redux store, change to connectedrouter

--- dotnet-mysql-application/ClientApp/boot.tsx
+++ dotnet-mysql-application/ClientApp/boot.tsx
@@ -4,20 +4,51 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import { BrowserRouter } from 'react-router-dom';
+import { ConnectedRouter } from 'react-router-redux';
 import * as RoutesModule from './router/routes';
+
+import configureStore from './configureStore';
+import { ApplicationState } from './store/';
+import { createBrowserHistory } from 'history';
+import { Provider } from 'react-redux';
 let routes = RoutesModule.routes;
 
 function renderApp() {
     // This code starts up the React app when it runs in a browser. It sets up the routing
     // configuration and injects the app into a DOM element.
+    // const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
     const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')!;
+    const history = createBrowserHistory({
+        basename: baseUrl,
+    });
+    const initialState = (window as any).initialReduxState as ApplicationState;
+    const store = configureStore(history, initialState);
     ReactDOM.render(
         <AppContainer>
-            <BrowserRouter children={ routes } basename={ baseUrl } />
+            <Provider store={store}>
+                <ConnectedRouter history={ history }>
+                    <div>
+                        {routes}
+                    </div>
+                </ConnectedRouter>
+            </Provider>
         </AppContainer>,
         document.getElementById('react-app')
     );
 }
+
+/* function renderApp() {
+    // This code starts up the React app when it runs in a browser. It sets up the routing
+    // configuration and injects the app into a DOM element.
+    const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href')!;
+    ReactDOM.render(
+        <AppContainer>
+        <BrowserRouter children={ routes } basename={ baseUrl } />
+        </AppContainer>,
+        document.getElementById('react-app')
+    );
+} */
+
 
 renderApp();
 
