de1291ec9f9086ed8813ef6e861231d253c20c29
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-07 13:28:17 +0300

    Fix route rendering for connected components

--- dotnet-mysql-application/ClientApp/router/routes.tsx
+++ dotnet-mysql-application/ClientApp/router/routes.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from '../layout';
-import { Home, FetchData, Counter, Loans, Equipments, Users, Register, Login } from '../pages'
+import { Home, FetchData, Counter, Loans, Equipments, Users, Register } from '../pages'
+import Login from '../pages/login/login';
 
 export const routes = <Layout>
-    <Route exact path='/' component={ Home } />
+{/*     <Route exact path='/' component={ Home } /> */}
+    <Route exact path='/' render={() => <Home/> } />
     <Route path='/counter' component={ Counter } />
     <Route path='/fetchdata' component={ FetchData } />
     <Route path='/loans' component={ Loans } />
     <Route path='/equipments' component={ Equipments } />
     <Route path='/users' component={ Users } />
-    <Route path='/register' component={ Register } />
-    <Route path='/login' component={ Login } />
+    <Route path='/login' component={ Login as any} />
 </Layout>;
