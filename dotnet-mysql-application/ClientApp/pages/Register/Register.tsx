9aebd2046d9d19dbfade35a03a1adbaf95a15ab7
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-07 13:28:58 +0300

    Apply styles to register page

--- dotnet-mysql-application/ClientApp/pages/Register/Register.tsx
+++ dotnet-mysql-application/ClientApp/pages/Register/Register.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import * as styles from './register.css';
+
+interface ILoginValidationError {
+    username?:string;
+    password?:string;
+}
+
+// Client side validation
+function validate(values:any) {
+    let errors:ILoginValidationError = {};
+    let hasErrors = false;
+    if (!values.username || values.username.trim() === '') {
+        errors.username = 'Enter username';
+        hasErrors = true;
+    }
+    if (!values.password || values.password.trim() === '') {
+        errors.password = 'Enter password';
+        hasErrors = true;
+    }
+    return hasErrors && errors;
+}
+
 
 export class Register extends React.Component<RouteComponentProps<{}>, {}> {
-    constructor() {
-        super();
+    constructor(props: any) {
+        super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
@@ -26,21 +48,21 @@ export class Register extends React.Component<RouteComponentProps<{}>, {}> {
     }
 
     public render() {
-        return <div>
-            <h1>Register</h1>
-
-            <p>Here should be a register user form.</p>
-            <form name="registerForm" onSubmit={this.handleSubmit}>
+        return <div className="col-md-6 col-md-offset-3">
+            <h2>Register</h2>
+            <div className={styles.registercontainer}>
+            <form name="registerForm" onSubmit={this.handleSubmit} className={styles.registerform}>
                 <label htmlFor="email">Enter email</label>
-                <input id="email" name="email" type="text" />
+                <input id="email" name="email" type="text" required/>
                 <label htmlFor="password">Enter password</label>
-                <input id="password" name="password" type="password" />
+                <input id="password" name="password" type="password" required/>
                 <label htmlFor="firstName">Enter first name</label>
-                <input id="firstName" name="firstName" type="text" />
+                <input id="firstName" name="firstName" type="text" required/>
                 <label htmlFor="lastName">Enter last name</label>
-                <input id="lastName" name="lastName" type="text" />
-                <button>Register!</button>
+                <input id="lastName" name="lastName" type="text" required/>
+                <button className={styles.registerbutton}>Register!</button>
             </form>
+            </div>
         </div>;
     }
 }
