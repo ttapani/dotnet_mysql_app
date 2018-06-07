bd9ff8ae8f20f3eea1094d6c4b6e8dc2b2a4d066
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-07 13:30:23 +0300

    Fix module updates not being detected

--- dotnet-mysql-application/webpack.config.js
+++ dotnet-mysql-application/webpack.config.js
@@ -3,13 +3,14 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CheckerPlugin = require('awesome-typescript-loader').CheckerPlugin;
 const bundleOutputDir = './wwwroot/dist';
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 module.exports = (env) => {
     const isDevBuild = !(env && env.prod);
     return [{
         stats: "verbose",
         entry: { 'main': './ClientApp/boot.tsx' },
-        resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+        resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx', '.css'] },
         output: {
             path: path.join(__dirname, bundleOutputDir),
             filename: '[name].js',
@@ -18,8 +19,10 @@ module.exports = (env) => {
         module: {
             rules: [
                 { test: /\.tsx?$/, include: /ClientApp/, use: 'awesome-typescript-loader?silent=false' },
-                { test: /\.tsx?$/, include: /pages/, use: 'awesome-typescript-loader?silent=false' },
-                { test: /\.css$/, use: isDevBuild ? ['style-loader', 'css-loader'] : ExtractTextPlugin.extract({ use: 'css-loader?minimize' }) },
+                // { test: /\.tsx?$/, include: /pages/, use: 'awesome-typescript-loader?silent=false' },
+                // { test: /\.tsx?$/, include: /navmenu/, use: 'awesome-typescript-loader?silent=false' },
+                // { test: /\.tsx?$/, include: /layout/, use: 'awesome-typescript-loader?silent=false' },
+                { test: /\.css$/, use: isDevBuild ? ['style-loader', 'typings-for-css-modules-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]&namedExport'] : ExtractTextPlugin.extract({ use: 'css-loader?minimize&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]&namedExport' }) },
                 { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
             ]
         },
@@ -28,7 +31,8 @@ module.exports = (env) => {
             new webpack.DllReferencePlugin({
                 context: __dirname,
                 manifest: require('./wwwroot/dist/vendor-manifest.json')
-            })
+            }),
+            new CaseSensitivePathsPlugin()
         ].concat(isDevBuild ? [
             // Plugins that apply in development builds only
             new webpack.SourceMapDevToolPlugin({
@@ -39,6 +43,14 @@ module.exports = (env) => {
             // Plugins that apply in production builds only
             new webpack.optimize.UglifyJsPlugin(),
             new ExtractTextPlugin('site.css')
-        ])
+        ]),
+        devServer: {
+            watchOptions: {
+              // if you're using Docker you may need this
+              aggregateTimeout: 300,
+              poll: 1000,
+              ignored: /node_modules/
+            },
+        }
     }];
 };
