8c501e1b2905393866055d9f9e0fbe82f82b3396
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-07 13:31:43 +0300

    Refactor login page to use redux for state

--- dotnet-mysql-application/ClientApp/pages/Login/Login.tsx
+++ dotnet-mysql-application/ClientApp/pages/Login/Login.tsx
@@ -1,43 +1,74 @@
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import { RouteComponentProps } from 'react-router';
 import * as styles from './login.css';
+import { signInUser, signInUserSuccess, signInUserFailure, userFromToken } from '../../store/user/actions';
+import { connect, Dispatch } from 'react-redux';
+import { UserState, UserSignInActions } from '../../store/user/types';
+import { ConnectedReduxProps, ApplicationState } from '../../store';
+import { AnyAction } from 'redux';
 
-export class Login extends React.Component<RouteComponentProps<{}>, {}> {
-    constructor() {
-        super();
+interface ILoginValidationError {
+    username?:string;
+    password?:string;
+}
+
+export interface LoginPageProps extends ConnectedReduxProps<UserState>, RouteComponentProps<any>, React.Props<any> {
+
+}
+
+type AllProps = LoginPageProps & UserState; 
+
+// Client side validation
+function validate(values:any) {
+    let errors:ILoginValidationError = {};
+    let hasErrors = false;
+    if (!values.username || values.username.trim() === '') {
+        errors.username = 'Enter username';
+        hasErrors = true;
+    }
+    if (!values.password || values.password.trim() === '') {
+        errors.password = 'Enter password';
+        hasErrors = true;
+    }
+    return hasErrors && errors;
+}
+
+class Login extends React.Component<AllProps, { }> {
+    constructor(props: AllProps) {
+        super(props);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit(event: any) {
+    handleSubmit(event: any): void {
         event.preventDefault();
+        console.log("login form submitted")
         const formData = new FormData(event.target);
-        const data = {
+        const credentials = {
             "username": formData.get("username"),
             "password": formData.get("password"),
         };
-        const headers = new Headers({'Content-type': 'application/json'});
-
-        fetch('api/auth', {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(data),
-        });
+        this.props.dispatch(signInUser(credentials));
     }
 
-    public render() {
+    public render(): React.ReactNode {
         return <div className="col-md-6 col-md-offset-3">
         <h2>Login</h2>
         <div className={styles.signincontainer}>
             <form name="loginForm" onSubmit={this.handleSubmit} className={styles.signinform}>
-                
+
                     <label htmlFor="username">Enter email</label>
                     <input id="username" name="username" type="text" required/>
                     <label htmlFor="password">Enter password</label>
                     <input id="password" name="password" type="password" required/>
                     <button className={styles.signinbutton}>Login!</button>
-               
+
             </form>
             </div>
         </div>;
     }
 }
+
+export default connect(
+    (state: ApplicationState) => state.user,
+)(Login);
