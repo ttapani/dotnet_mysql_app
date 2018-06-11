9f7cc5ea01ac1cffff95c9d5f675c239141531bd
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-11 16:18:07 +0300

    Implement sagas for login, failure and success

--- dotnet-mysql-application/ClientApp/store/user/sagas.ts
+++ dotnet-mysql-application/ClientApp/store/user/sagas.ts
@@ -1,9 +1,11 @@
 import { call, put, takeLatest, all } from 'redux-saga/effects';
-import { SignInUserAction, UserCredentials, UserToken } from './types';
+import { SignInUserAction, UserCredentials, UserToken, SignInUserFailureAction, SignInUserSuccessAction } from './types';
 import { Action } from 'redux';
 import { signInUserSuccess, signInUserFailure } from './actions';
+import { push } from 'react-router-redux';
 
 // loansystem-API
+// This will be moved to services
 function* signinUserApi (credentials: UserCredentials) {
   console.log("entered promise creator");
   const url = "http://127.0.0.1:5001/api/auth";
@@ -23,26 +25,49 @@ export function* signInUserAsync(action: SignInUserAction) {
   try {
     const userToken = yield call(signinUserApi, action.payload.credentials);
     console.log(userToken)
-    yield put(signInUserSuccess(userToken));
+    if(userToken.hasOwnProperty('auth_token')) {
+      sessionStorage.setItem('jwtToken', userToken.auth_token);
+      yield put(signInUserSuccess(userToken));
+    } else {
+      if('Password' in userToken) {
+        const {Password} = userToken;
+        throw new Error(Password);
+      } else if('login_failure' in userToken) {
+        const {login_failure} = userToken;
+        throw new Error(login_failure);
+      }
+    }
   } catch (err) {
-    yield put(signInUserFailure(err));
+    console.log(err.message);
+    yield put(signInUserFailure(err.message));
   }
-/*   if(userToken.hasOwnProperty('auth_token')) {
-    console.log("success");
-    yield put({ type: '@@user/SIGNIN_SUCCESS', userToken});
-  } else {
-    console.log("failure");
-    yield put({ type: '@@user/SIGNIN_FAILURE', payload: 'error'});
-  } */
 }
 
-// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
 export function* watchSignInUser() {
   yield takeLatest('@@user/SIGNIN', signInUserAsync);
 }
 
+export function* alertUser(action: SignInUserFailureAction) {
+  console.log("failure saga entered");
+  yield call(window.alert, action.payload.message);
+}
+
+export function* watchSignInUserFailure() {
+  yield takeLatest('@@user/SIGNIN_FAILURE', alertUser);
+}
+
+export function* signInUser(action: SignInUserSuccessAction) {
+  yield put(push('/'));
+}
+
+export function* watchSignInUserSuccess() {
+  yield takeLatest('@@user/SIGNIN_SUCCESS', signInUser);
+}
+
 export default function* userSagas() {
   yield all([
-    watchSignInUser()
+    watchSignInUser(),
+    watchSignInUserFailure(),
+    watchSignInUserSuccess(),
   ])
 }
