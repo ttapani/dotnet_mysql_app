cdd4240daedccaf54f66932bf2104fc0fb02c2bc
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-12 16:14:34 +0300

    Add logout

--- dotnet-mysql-application/ClientApp/navMenu/navMenu.tsx
+++ dotnet-mysql-application/ClientApp/navMenu/navMenu.tsx
@@ -1,29 +1,34 @@
 import * as React from 'react';
-import { Link, NavLink, RouteComponentProps } from 'react-router-dom';
+import { Link, NavLink, RouteComponentProps, withRouter } from 'react-router-dom';
 import * as styles from './navMenu.css';
-import { connect } from 'react-redux';
-import { ApplicationState } from '../store';
-import { LoginState } from '../store/user/types';
+import { connect, Dispatch } from 'react-redux';
+import { ApplicationState, ConnectedReduxProps } from '../store';
+import { LoginState, LogOutUserAction } from '../store/user/types';
+import { logOutUser } from '../store/user/actions';
 
 {/* NavMenu is not a route component, don't give it RouteComponentProps!!! */}
-export interface NavMenuProps {
-    isLoggedIn: boolean,
-}
-
-interface State {
+export interface NavMenuProps extends ConnectedReduxProps<LoginState>, React.Props<any>  {
+    isLoggedIn?: boolean,
 }
 
 type AllProps = NavMenuProps & LoginState; 
 
-class NavMenu extends React.Component<AllProps, State> {
+class NavMenu extends React.Component<AllProps> {
     constructor(props: AllProps) {
         super(props); 
-        this.state = {
-        };
+        this.handleLogout = this.handleLogout.bind(this);
     }
-    
-    readonly state: State = {
+
+    public static defaultProps: Partial<AllProps> = {
+        isLoggedIn: false,
     };
+
+    private handleLogout(event: any) {
+        event.preventdefault;
+        if(this.props.dispatch) {
+            this.props.dispatch(logOutUser());
+        }
+    }
 
     public render() {
         return <div className={styles.mainNav}>
@@ -70,13 +75,7 @@ class NavMenu extends React.Component<AllProps, State> {
                             <NavLink to={ '/users' } activeClassName='active'>
                                 <span className='glyphicon glyphicon-th-list'></span> Users
                             </NavLink>
-                        </li> */}
-                        {/* <li>
-                            <NavLink to={ '/register' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-flash'></span> Register
-                            </NavLink>
-                        </li> */}
-                        
+                        </li> */}  
                     </ul>
                     <ul className='nav navbar-nav navbar-right'>
                         <li className={this.props.isLoggedIn ? "hidden" : ""}>
@@ -90,7 +89,7 @@ class NavMenu extends React.Component<AllProps, State> {
                             </NavLink>
                         </li>
                         <li className={!this.props.isLoggedIn ? "hidden" : ""}>
-                            <NavLink to={ '/logout' } activeClassName='active'>
+                            <NavLink to={ '#' } activeClassName='active' onClick={ this.handleLogout }>
                                 <span className='glyphicon glyphicon-log-in'></span> Logout
                             </NavLink>
                         </li>
@@ -103,9 +102,14 @@ class NavMenu extends React.Component<AllProps, State> {
 }
 
 const mapStateToProps = (state: ApplicationState) => ({
-    isLoggedIn: state.login.loggedIn
+    isLoggedIn: state.login.loggedIn,
 });
 
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+    dispatch: dispatch
+})
+
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    null,
 )(NavMenu);
