b95ad3f5c7b967f079f7ede50a22a3265502901d
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-14 14:55:41 +0300

    tslint, cleanups

--- dotnet-mysql-application/ClientApp/pages/Register/Register.tsx
+++ dotnet-mysql-application/ClientApp/pages/Register/Register.tsx
@@ -2,27 +2,6 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import * as styles from './register.css';
 
-interface ILoginValidationError {
-    username?:string;
-    password?:string;
-}
-
-// Client side validation
-function validate(values:any) {
-    let errors:ILoginValidationError = {};
-    let hasErrors = false;
-    if (!values.username || values.username.trim() === '') {
-        errors.username = 'Enter username';
-        hasErrors = true;
-    }
-    if (!values.password || values.password.trim() === '') {
-        errors.password = 'Enter password';
-        hasErrors = true;
-    }
-    return hasErrors && errors;
-}
-
-
 export class Register extends React.Component<RouteComponentProps<{}>, {}> {
     constructor(props: any) {
         super(props);
@@ -33,10 +12,10 @@ export class Register extends React.Component<RouteComponentProps<{}>, {}> {
         event.preventDefault();
         const formData = new FormData(event.target);
         const data = {
-            "email": formData.get("email"),
-            "password": formData.get("password"),
-            "firstName": formData.get("firstName"),
-            "lastName": formData.get("lastName"),
+            email: formData.get('email'),
+            password: formData.get('password'),
+            firstName: formData.get('firstName'),
+            lastName: formData.get('lastName'),
         };
         const headers = new Headers({'Content-type': 'application/json'});
 
@@ -48,21 +27,23 @@ export class Register extends React.Component<RouteComponentProps<{}>, {}> {
     }
 
     public render() {
-        return <div className="col-md-6 col-md-offset-3">
+        return (
+            <div className="col-md-6 col-md-offset-3">
             <h2>Register</h2>
             <div className={styles.registercontainer}>
             <form name="registerForm" onSubmit={this.handleSubmit} className={styles.registerform}>
                 <label htmlFor="email">Enter email</label>
-                <input id="email" name="email" type="text" required/>
+                <input id="email" name="email" type="text" required={true}/>
                 <label htmlFor="password">Enter password</label>
-                <input id="password" name="password" type="password" required/>
+                <input id="password" name="password" type="password" required={true}/>
                 <label htmlFor="firstName">Enter first name</label>
-                <input id="firstName" name="firstName" type="text" required/>
+                <input id="firstName" name="firstName" type="text" required={true}/>
                 <label htmlFor="lastName">Enter last name</label>
-                <input id="lastName" name="lastName" type="text" required/>
+                <input id="lastName" name="lastName" type="text" required={true}/>
                 <button className={styles.registerbutton}>Register!</button>
             </form>
             </div>
-        </div>;
+        </div>
+        );
     }
 }
