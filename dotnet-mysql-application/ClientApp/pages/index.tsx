ff86fce0b3f2318974d6970e1784fae323762163
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-14 16:00:30 +0300

    Fix export

--- dotnet-mysql-application/ClientApp/pages/index.tsx
+++ dotnet-mysql-application/ClientApp/pages/index.tsx
@@ -2,7 +2,7 @@ export { default as Home } from './home/home';
 export * from './fetchData/fetchData';
 export * from './counter/counter';
 export * from './loans/loans';
-export * from './equipments/equipments';
+export { default as Items } from './items/items';
 export * from './users/users';
 export * from './register/register';
 export { default as Login } from './login/Login';
