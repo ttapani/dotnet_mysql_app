a8b328ed7a0b077d18c9fd447efcbc13ef53f1d1
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-14 14:19:50 +0300

    tslint, cleanups

--- dotnet-mysql-application/ClientApp/router/routes.tsx
+++ dotnet-mysql-application/ClientApp/router/routes.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from '../layout';
-import { Home, FetchData, Counter, Loans, Equipments, Users, Register, Login } from '../pages'
+import { Home, FetchData, Counter, Loans, Equipments, Users, Register, Login } from '../pages';
 
-export const routes = <Layout>
-{/*     <Route exact path='/' component={ Home } /> */}
-    <Route exact path='/' render={() => <Home/> } />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
-    <Route path='/loans' component={ Loans } />
-    <Route path='/equipments' component={ Equipments } />
-    <Route path='/users' component={ Users } />
-    <Route path='/login' component={ Login } />
-</Layout>;
+export const routes = (
+    <Layout>
+        <Route exact={true} path="/" component={Home} />
+        <Route path="/counter" component={Counter} />
+        <Route path="/fetchdata" component={FetchData} />
+        <Route path="/loans" component={Loans} />
+        <Route path="/equipments" component={Equipments} />
+        <Route path="/users" component={Users} />
+        <Route path="/login" component={Login} />
+    </Layout>);
