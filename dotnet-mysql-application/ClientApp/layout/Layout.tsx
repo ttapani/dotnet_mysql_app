0a77da053f5f645a2fba89f23fce40c7f1632b77
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-14 11:02:59 +0300

    Fix typo

--- dotnet-mysql-application/ClientApp/layout/Layout.tsx
+++ dotnet-mysql-application/ClientApp/layout/Layout.tsx
@@ -8,7 +8,7 @@ export interface LayoutProps {
 export class Layout extends React.Component<LayoutProps, {}> {
     public render() {
         return <div>
-            <NavMenu/>
+            <NavMenu />
             <div className='container-fluid'>
                 { this.props.children }
             </div>
