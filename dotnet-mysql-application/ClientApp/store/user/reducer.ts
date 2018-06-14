cc9ccc0133e03b564929ec8362bd278970d66cd5
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-14 14:45:28 +0300

    Reducer sets user name

--- dotnet-mysql-application/ClientApp/store/user/reducer.ts
+++ dotnet-mysql-application/ClientApp/store/user/reducer.ts
@@ -1,5 +1,5 @@
 import { Reducer } from 'redux';
-import { LoginState, UserActions, UserInfo } from './types';
+import { LoginState, UserActions } from './types';
 
 export const initialState: LoginState = {
   loggedIn: false,
@@ -11,11 +11,15 @@ const reducer: Reducer<LoginState> = (state: LoginState = initialState, action) 
     case '@@user/SIGNIN':
       return { ...state, loggedIn: false, credentials: action.payload.credentials, isLoggingIn: true };
     case '@@user/SIGNIN_SUCCESS':
-      return { ...state, loggedIn: true, token: action.payload.token, isLoggingIn: false };
+      return { ...state,
+        loggedIn: true,
+        token: action.payload.token.auth_token,
+        isLoggingIn: false,
+        userName: action.payload.token.id };
     case '@@user/SIGNIN_FAILURE':
       return { ...state, loggedIn: false, message: action.payload.message, isLoggingIn: false };
     case '@@user/LOGOUT':
-      return { ...state, loggedIn: false };
+      return { ...state, loggedIn: false, userName: 'guest' };
     default:
       return state;
   }
