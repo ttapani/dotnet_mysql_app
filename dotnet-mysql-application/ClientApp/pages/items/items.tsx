d98ec76818dd7e714c9898b6afba0919c0d2fa28
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-14 14:56:38 +0300

    Refactor app structure

--- dotnet-mysql-application/ClientApp/pages/items/items.tsx
+++ dotnet-mysql-application/ClientApp/pages/items/items.tsx
@@ -1,17 +1,19 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 
-export class Equipments extends React.Component<RouteComponentProps<{}>, {}> {
+export default class Items extends React.Component<RouteComponentProps<{}>, {}> {
     constructor(props: any) {
         super(props);
     }
 
     public render() {
-        return <div>
-            <h1>Equipments</h1>
+        return (
+        <div>
+            <h1>Items</h1>
 
             <p>This is a simple example of a React component.</p>
 
-        </div>;
+        </div>
+        );
     }
 }
