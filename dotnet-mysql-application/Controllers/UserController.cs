85f754784c5dd11eb60e19260f523964fab2337e
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-14 13:43:34 +0300

    Refactor usercontroller for future use

--- dotnet-mysql-application/Controllers/UserController.cs
+++ dotnet-mysql-application/Controllers/UserController.cs
@@ -2,9 +2,13 @@
 // using Microsoft.AspNetCore.Mvc;
 // using Microsoft.EntityFrameworkCore;
 // using dotnet_mysql_application.Models;
+// using Microsoft.AspNetCore.Authorization;
+// using System.Threading.Tasks;
+// using Microsoft.AspNetCore.Authentication;
 
 // namespace dotnet_mysql_application.Controllers
 // {
+//     [Authorize(Policy = "ApiUser")]
 //     [Route("api/v1/[controller]")]
 //     [Produces("application/json")]
 //     public class UserController : Controller
@@ -16,71 +20,83 @@
 //             db = context;
 //         }
 
-//         [HttpGet("{id}")]
+//         [HttpGet]
 //         [ProducesResponseType(200)]
 //         [ProducesResponseType(404)]
-//         public IActionResult GetUserById(string id)
+//         public async Task<IActionResult> GetCurrentUser()
 //         {
-//             var user = db.Users.SingleOrDefault(u => u.Id.ToString() == id);
-//             if (user == null)
+//             string accessToken = await HttpContext.GetTokenAsync("access_token");
+//             var user = await db.Users.FirstOrDefaultAsync(e => e. == id);
+//             if (user == null)   
 //                 return NotFound();
 //             return new OkObjectResult(user);
 //         }
 
-//         [HttpGet]
-//         [ProducesResponseType(200)]
-//         [ProducesResponseType(404)]
-//         public IActionResult GetAllUsers()
-//         {
-//             var users = db.Users.ToList();
-//             if (users == null)
-//                 return NotFound();
-//             return new OkObjectResult(users);
-//         }
+        // [HttpGet("{id}")]
+        // [ProducesResponseType(200)]
+        // [ProducesResponseType(404)]
+        // public IActionResult GetUserById(string id)
+        // {
+        //     var user = db.Users.SingleOrDefault(u => u.Id.ToString() == id);
+        //     if (user == null)
+        //         return NotFound();
+        //     return new OkObjectResult(user);
+        // }
 
-//         [HttpPost]
-//         [ProducesResponseType(201, Type = typeof(User))]
-//         [ProducesResponseType(400)]
-//         public IActionResult AddUser([FromBody] User user)
-//         {
-//             if(ModelState.IsValid) {
-//                 db.Users.Add(user);
-//                 db.SaveChanges();
-//                 return CreatedAtAction(nameof(GetUserById), new { id = user.Id },  user);
-//             }
-//             else
-//             return BadRequest();
-//         }
+        // [HttpGet]
+        // [ProducesResponseType(200)]
+        // [ProducesResponseType(404)]
+        // public IActionResult GetAllUsers()
+        // {
+        //     var users = db.Users.ToList();
+        //     if (users == null)
+        //         return NotFound();
+        //     return new OkObjectResult(users);
+        // }
 
-//         [HttpPut("{id}")]
-//         [ProducesResponseType(200)]
-//         [ProducesResponseType(400)]
-//         public IActionResult UpdateUser(string id, [FromBody] User user)
-//         {
-//             var targetUser = db.Users.SingleOrDefault(u => u.Id.ToString() == id);
-//             if (targetUser == null)
-//                 return NotFound();
-//             else if(ModelState.IsValid) {
-//                 targetUser.Name = user.Name;
-//                 db.Users.Update(targetUser);
-//                 db.SaveChanges();
-//                 return Ok(user);
-//             }
-//             else
-//                 return BadRequest();
-//         }
+        // [HttpPost]
+        // [ProducesResponseType(201, Type = typeof(User))]
+        // [ProducesResponseType(400)]
+        // public IActionResult AddUser([FromBody] User user)
+        // {
+        //     if(ModelState.IsValid) {
+        //         db.Users.Add(user);
+        //         db.SaveChanges();
+        //         return CreatedAtAction(nameof(GetUserById), new { id = user.Id },  user);
+        //     }
+        //     else
+        //     return BadRequest();
+        // }
 
-//         [HttpDelete("{id}")]
-//         [ProducesResponseType(204)]
-//         [ProducesResponseType(400)]
-//         public IActionResult DeleteUser(string id)
-//         {
-//             var user = db.Users.SingleOrDefault(u => u.Id.ToString() == id);
-//             if (user == null)
-//                 return NotFound();
-//             db.Remove(user);
-//             db.SaveChanges();
-//             return new NoContentResult();
-//         }
+        // [HttpPut("{id}")]
+        // [ProducesResponseType(200)]
+        // [ProducesResponseType(400)]
+        // public IActionResult UpdateUser(string id, [FromBody] User user)
+        // {
+        //     var targetUser = db.Users.SingleOrDefault(u => u.Id.ToString() == id);
+        //     if (targetUser == null)
+        //         return NotFound();
+        //     else if(ModelState.IsValid) {
+        //         targetUser.Name = user.Name;
+        //         db.Users.Update(targetUser);
+        //         db.SaveChanges();
+        //         return Ok(user);
+        //     }
+        //     else
+        //         return BadRequest();
+        // }
+
+        // [HttpDelete("{id}")]
+        // [ProducesResponseType(204)]
+        // [ProducesResponseType(400)]
+        // public IActionResult DeleteUser(string id)
+        // {
+        //     var user = db.Users.SingleOrDefault(u => u.Id.ToString() == id);
+        //     if (user == null)
+        //         return NotFound();
+        //     db.Remove(user);
+        //     db.SaveChanges();
+        //     return new NoContentResult();
+        // }
 //     }
 // }
