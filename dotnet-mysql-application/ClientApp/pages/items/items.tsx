ab09516914d75293ec96f968e1276d69f7616bdb
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-19 09:30:39 +0300

    Add initial support for showing all items

--- dotnet-mysql-application/ClientApp/pages/items/items.tsx
+++ dotnet-mysql-application/ClientApp/pages/items/items.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { FetchItemsState } from '../../store/item/types';
+import { getItems } from '../../store/item/actions';
+import { connect } from 'react-redux';
+import { ApplicationState } from '../../store';
 
-export default class Items extends React.Component<RouteComponentProps<{}>, {}> {
-    constructor(props: any) {
+type ItemsProps = FetchItemsState & typeof getItems & RouteComponentProps<{}>;
+
+class Items extends React.Component<ItemsProps, {}> {
+    constructor(props: ItemsProps) {
         super(props);
+    }
+
+    public componentWillMount(): void {
+        console.log(this.props);
+        this.props.getItems();
     }
 
     public render() {
@@ -11,9 +22,24 @@ export default class Items extends React.Component<RouteComponentProps<{}>, {}> 
         <div>
             <h1>Items</h1>
 
-            <p>This is a simple example of a React component.</p>
+            <p>Trying to get some data from API with AJAX.</p>
+
+            {this.renderItemsList()}
 
         </div>
         );
     }
+
+    public renderItemsList() {
+        return (
+            <ul>
+                {this.props.items.map(item => <li key={item.guid}>{item.name}</li>)}
+            </ul>
+        );
+    }
 }
+
+export default connect(
+    (state: ApplicationState) => state.items,
+    {getItems}
+)(Items);
