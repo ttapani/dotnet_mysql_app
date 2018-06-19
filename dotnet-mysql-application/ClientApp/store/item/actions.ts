76b70c2f458840264623711d4531c637c3c27769
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-19 13:59:00 +0300

    Add actioncreators for addItem

--- dotnet-mysql-application/ClientApp/store/item/actions.ts
+++ dotnet-mysql-application/ClientApp/store/item/actions.ts
@@ -1,5 +1,6 @@
-import { ActionCreator, Action } from 'redux';
+import { ActionCreator } from 'redux';
 import { GetItemsAction, GetItemsSuccessAction, GetItemsFailureAction, Item } from './types';
+import { AddItemAction, AddItemSuccessAction, AddItemFailureAction } from './types';
 
 export const getItems: ActionCreator<GetItemsAction> = () => ({
     type: '@@items/GET',
@@ -14,6 +15,24 @@ export const getItemsSuccess: ActionCreator<GetItemsSuccessAction> = (items: Ite
 
 export const getItemsFailure: ActionCreator<GetItemsFailureAction> = (error: string) => ({
     type: '@@items/GET_FAILURE',
+    payload: {
+        error,
+    },
+});
+
+export const addItem: ActionCreator<AddItemAction> = (item: Item) => ({
+    type: '@@items/ADD',
+    payload: {
+        item,
+    },
+});
+
+export const addItemSuccess: ActionCreator<AddItemSuccessAction> = () => ({
+    type: '@@items/ADD_SUCCESS',
+});
+
+export const addItemFailure: ActionCreator<AddItemFailureAction> = (error: string) => ({
+    type: '@@items/ADD_FAILURE',
     payload: {
         error,
     },
