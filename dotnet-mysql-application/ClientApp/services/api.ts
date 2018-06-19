e0111a347a8b89ccb514693c64936489d9248ba8
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-19 09:28:58 +0300

    Add initial api support for JWT, items

--- dotnet-mysql-application/ClientApp/services/api.ts
+++ dotnet-mysql-application/ClientApp/services/api.ts
@@ -1,8 +1,8 @@
 import { UserCredentials } from '../store/user/types';
 
 const ROOT_URL = location.href.indexOf('localhost') > 0 ? 'http://localhost:3000/api' : '/api';
-const HEADERS = { 'content-type': 'application/json'};
-const jwt = '';
+const jwt = 'Bearer: ' + sessionStorage.getItem('jwtToken') || '';
+const HEADERS = { 'content-type': 'application/json', 'Authorization': jwt};
 
 export function signInUserApi(credentials: UserCredentials) {
     console.log('entered promise creator in api');
@@ -11,3 +11,11 @@ export function signInUserApi(credentials: UserCredentials) {
       {body: JSON.stringify(credentials), headers: HEADERS, method: 'POST'});
     return promise;
 }
+
+export const getItems = () => {
+    console.log('entered item promise creator in api');
+    const url = ROOT_URL + '/v1/items';
+    const promise = fetch(url,
+      {headers: HEADERS, method: 'GET'});
+    return promise;
+};
