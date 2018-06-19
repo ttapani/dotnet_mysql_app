9af8946e39c0b7d7d7263787dc1c20aded02e8bc
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-19 11:01:16 +0300

    Rework header generation

--- dotnet-mysql-application/ClientApp/services/api.ts
+++ dotnet-mysql-application/ClientApp/services/api.ts
@@ -1,14 +1,25 @@
 import { UserCredentials } from '../store/user/types';
 
 const ROOT_URL = location.href.indexOf('localhost') > 0 ? 'http://localhost:3000/api' : '/api';
-const jwt = 'Bearer: ' + sessionStorage.getItem('jwtToken') || '';
-const HEADERS = { 'content-type': 'application/json', 'Authorization': jwt};
+
+const getJWT = () => {
+  if (sessionStorage != null) {
+    const token = sessionStorage.getItem('jwtToken');
+    return 'Bearer ' + token;
+  } else {
+    return '';
+  }
+};
+
+const getHeaders = () => {
+  return { 'content-type': 'application/json', 'Authorization': getJWT()};
+};
 
 export function signInUserApi(credentials: UserCredentials) {
     console.log('entered promise creator in api');
     const url = ROOT_URL + '/auth';
     const promise = fetch(url,
-      {body: JSON.stringify(credentials), headers: HEADERS, method: 'POST'});
+      {body: JSON.stringify(credentials), headers: getHeaders(), method: 'POST'});
     return promise;
 }
 
@@ -16,6 +27,6 @@ export const getItems = () => {
     console.log('entered item promise creator in api');
     const url = ROOT_URL + '/v1/items';
     const promise = fetch(url,
-      {headers: HEADERS, method: 'GET'});
+      {headers: getHeaders(), method: 'GET'});
     return promise;
 };
