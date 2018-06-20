4aab0fa4bedca3fb57f10094abf6a96b1b21179f
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-20 11:27:37 +0300

    Add actions for removing items

--- dotnet-mysql-application/ClientApp/store/item/actions.ts
+++ dotnet-mysql-application/ClientApp/store/item/actions.ts
@@ -1,6 +1,7 @@
 import { ActionCreator } from 'redux';
 import { GetItemsAction, GetItemsSuccessAction, GetItemsFailureAction, Item } from './types';
 import { AddItemAction, AddItemSuccessAction, AddItemFailureAction } from './types';
+import { DeleteItemAction, DeleteItemSuccessAction, DeleteItemFailureAction } from './types';
 
 export const getItems: ActionCreator<GetItemsAction> = () => ({
     type: '@@items/GET',
@@ -33,6 +34,24 @@ export const addItemSuccess: ActionCreator<AddItemSuccessAction> = () => ({
 
 export const addItemFailure: ActionCreator<AddItemFailureAction> = (error: string) => ({
     type: '@@items/ADD_FAILURE',
+    payload: {
+        error,
+    },
+});
+
+export const deleteItem: ActionCreator<DeleteItemAction> = (item: Item) => ({
+    type: '@@items/DELETE',
+    payload: {
+        item,
+    },
+});
+
+export const deleteItemSuccess: ActionCreator<DeleteItemSuccessAction> = () => ({
+    type: '@@items/DELETE_SUCCESS',
+});
+
+export const deleteItemFailure: ActionCreator<DeleteItemFailureAction> = (error: string) => ({
+    type: '@@items/DELETE_FAILURE',
     payload: {
         error,
     },
