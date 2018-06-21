e7cf474af6ec23eaa4bd9a1e9eaadedb77a924f3
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-21 10:16:01 +0300

    Add frontend support for editing

--- dotnet-mysql-application/ClientApp/pages/items/containers/itemRow.tsx
+++ dotnet-mysql-application/ClientApp/pages/items/containers/itemRow.tsx
@@ -1,18 +1,25 @@
 import * as React from 'react';
 import { Item } from '../../../store/item/types';
 import { connect, Dispatch } from 'react-redux';
-import { deleteItem } from '../../../store/item/actions';
+import { deleteItem, updateItem } from '../../../store/item/actions';
 
 interface ItemRowProps {
     item: Item;
+    updateItem: (item: Item) => any;
     deleteItem: (item: Item) => any;
+}
+
+interface ItemRowState {
+    isEditing: boolean;
+    newName: string;
 }
 
 type AllProps = ItemRowProps & typeof deleteItem;
 
-class ItemRow extends React.Component<AllProps> {
+class ItemRow extends React.Component<AllProps, ItemRowState> {
     constructor(props: AllProps) {
         super(props);
+        this.state = { isEditing: false, newName: this.props.item.name };
     }
 
     onDelete = (event: any): void => {
@@ -21,15 +28,59 @@ class ItemRow extends React.Component<AllProps> {
         this.props.deleteItem(item);
     }
 
+    onEdit = (event: any): void => {
+        event.preventDefault();
+        this.setState({ isEditing: true });
+    }
+
+    onNameChanged = (event: any): void => {
+        this.setState({ newName: event.target.value });
+    }
+
+    onSave = (event: any): void => {
+        const { item } = this.props;
+        item.name = this.state.newName;
+        event.preventDefault();
+        this.setState({ isEditing: false });
+        this.props.updateItem(item);
+    }
+
+    onCancel = (event: any): void => {
+        event.preventDefault();
+        this.setState({ isEditing: false });
+    }
+
     public render() {
+        if (!this.state.isEditing) {
+            return this.renderShow();
+        } else {
+            return this.renderEdit();
+        }
+    }
+
+    private renderShow() {
         const { item: { name } } = this.props;
         return (
-            <div>{name}<a href="items/#" onClick={this.onDelete}>Delete</a></div>
+            <div>{name}
+                <a href="items/#" onClick={this.onEdit}>Edit</a>
+                <a href="items/#" onClick={this.onDelete}>Delete</a>
+            </div>
+        );
+    }
+
+    private renderEdit() {
+        return (
+            <div>
+                <input type="text" name="itemName" defaultValue={this.state.newName} onChange={this.onNameChanged}/>
+                <a href="items/#" onClick={this.onSave}>Save</a>
+                <a href="items/#" onClick={this.onCancel}>Cancel</a>
+            </div>
         );
     }
 }
 
 export default connect(undefined,
     (dispatch: Dispatch) => ({
-        deleteItem: (item: Item) => dispatch(deleteItem(item))})
+        deleteItem: (item: Item) => dispatch(deleteItem(item)),
+        updateItem: (item: Item) => dispatch(updateItem(item))})
 )(ItemRow);
