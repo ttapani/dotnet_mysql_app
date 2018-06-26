6ac32196c3cafc637242c83a1374cdc5b0b7bd24
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-26 10:27:05 +0300

    Map default value for items

--- dotnet-mysql-application/Models/LoanSystemContext.cs
+++ dotnet-mysql-application/Models/LoanSystemContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace dotnet_mysql_application.Models
 {
-    public class LoanSystemContext : IdentityDbContext<LoanSystemUser>
+    public partial class LoanSystemContext : IdentityDbContext<LoanSystemUser>
     {
         public LoanSystemContext(DbContextOptions options)
             : base(options)
@@ -15,6 +15,14 @@ namespace dotnet_mysql_application.Models
         //public DbSet<User> Users { get; set; }
 
         public DbSet<Loan> Loans { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Item>()
+                .Property(i => i.IsAvailable)
+                .HasDefaultValue(true);
+        }
 
         //public DbSet<Role> Roles { get; set; }
     }
