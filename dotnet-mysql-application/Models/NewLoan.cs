699d3ce7b70fa74c236c2002ebba48d1912dcb4c
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-26 10:26:40 +0300

    Add model for basic loans

---
+++ dotnet-mysql-application/Models/NewLoan.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotnet_mysql_application.Models
+{
+    public class NewLoan
+    {
+        public string id { get; set; }
+    }
+}
