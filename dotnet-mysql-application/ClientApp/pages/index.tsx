93b38b56ca802d816cb38503c31735de38e3b906
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-26 11:33:56 +0300

    Fix import/export statement

--- dotnet-mysql-application/ClientApp/pages/index.tsx
+++ dotnet-mysql-application/ClientApp/pages/index.tsx
@@ -1,7 +1,7 @@
 export { default as Home } from './home/home';
 export * from './fetchData/fetchData';
 export * from './counter/counter';
-export * from './loans/loans';
+export { default as Loans } from './loans/loans';
 export { default as Items } from './items/items';
 export * from './users/users';
 export * from './register/register';
