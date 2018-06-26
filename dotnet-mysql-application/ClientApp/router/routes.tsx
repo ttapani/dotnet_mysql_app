e908615583fc822b662de83e7e3489493a1c294e
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-26 10:31:12 +0300

    Add new pages to routes

--- dotnet-mysql-application/ClientApp/router/routes.tsx
+++ dotnet-mysql-application/ClientApp/router/routes.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from '../layout';
-import { Home, FetchData, Counter, Loans, Items, Users, Register, Login } from '../pages';
+import { Home, FetchData, Counter, Loans, Items, Users, Register, Login, ManageItems } from '../pages';
 
 export const routes = (
     <Layout>
         <Route exact={true} path="/" component={Home} />
+        <Route path="/manage" component={ManageItems} />
         <Route path="/items" component={Items} />
         <Route path="/login" component={Login} />
     </Layout>);
