96c1b0c902f549bee3e7537f48c739cc8494f073
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-26 10:30:46 +0300

    Update combined reducers

--- dotnet-mysql-application/ClientApp/store/index.ts
+++ dotnet-mysql-application/ClientApp/store/index.ts
@@ -5,16 +5,20 @@ import { LoginState } from './user/types';
 import userReducer from './user/reducer';
 import itemsReducer from './item/reducer';
 import { FetchItemsState } from './item/types';
+import { LoansState } from './loan/types';
+import loansReducer from './loan/reducer';
 
 export interface ApplicationState {
     login: LoginState;
     items: FetchItemsState;
+    loans: LoansState;
 }
 
 export const reducers: Reducer<ApplicationState> = combineReducers<ApplicationState>({
     route: routerReducer,
     login: userReducer,
     items: itemsReducer,
+    loans: loansReducer,
 });
 
 export interface ConnectedReduxProps<S> {
