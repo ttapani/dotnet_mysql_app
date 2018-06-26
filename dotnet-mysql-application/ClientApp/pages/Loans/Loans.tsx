53ed8bef87df647e2ed86c8fcbbf3510ac23a8d0
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-26 11:34:58 +0300

    Initial support for listing loans

--- dotnet-mysql-application/ClientApp/pages/Loans/Loans.tsx
+++ dotnet-mysql-application/ClientApp/pages/Loans/Loans.tsx
@@ -1,9 +1,24 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { connect, Dispatch } from 'react-redux';
+import { ApplicationState } from '../../store';
+import { getLoans } from '../../store/loan/actions';
+import { LoansState } from '../../store/loan/types';
 
-export class Loans extends React.Component<RouteComponentProps<{}>, {}> {
-    constructor(props: any) {
+interface LoansPageProps {
+    getLoans: () => any;
+}
+
+type AllProps = LoansPageProps & LoansState & typeof getLoans & RouteComponentProps<{}>;
+
+class Loans extends React.Component<AllProps> {
+    constructor(props: AllProps) {
         super(props);
+    }
+
+    public componentWillMount(): void {
+        console.log(this.props);
+        this.props.getLoans();
     }
 
     public render() {
@@ -11,9 +26,24 @@ export class Loans extends React.Component<RouteComponentProps<{}>, {}> {
         <div>
             <h1>Loans</h1>
 
-            <p>This is a simple example of a React component.</p>
+            {this.renderLoansList()}
 
         </div>
         );
     }
+
+    private renderLoansList() {
+        return (
+            <ul>
+                {this.props.loans.map(loan => <li key={loan.id}>{JSON.stringify(loan)}</li>)}
+            </ul>
+        );
+    }
 }
+
+export default connect(
+    (state: ApplicationState) => state.loans,
+    (dispatch: Dispatch) => ({
+        getLoans: () => dispatch(getLoans()),
+    })
+)(Loans);
