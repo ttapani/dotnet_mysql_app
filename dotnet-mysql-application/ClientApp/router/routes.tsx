c8643cbab17d700bc4ef9267a96500b1b28f201f
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-27 11:00:54 +0300

    Protect pages with authentication on client side

--- dotnet-mysql-application/ClientApp/router/routes.tsx
+++ dotnet-mysql-application/ClientApp/router/routes.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
-import { Layout } from '../layout';
 import { Home, FetchData, Counter, Loans, Items, Users, Register, Login, ManageItems } from '../pages';
+import { ApplicationState } from '../store';
+import { connect } from 'react-redux';
+import withAuthentication from './withAuthentication';
+
+const mapStateToProps = (state: ApplicationState) => ({loggedIn: state.login.loggedIn});
 
 export const routes = (
-    <Layout>
+    <React.Fragment>
         <Route exact={true} path="/" component={Home} />
-        <Route path="/manage" component={ManageItems} />
-        <Route path="/items" component={Items} />
+        <Route path="/manage" component={connect(mapStateToProps, null)(withAuthentication(ManageItems))} />
+        <Route path="/items" component={connect(mapStateToProps, null)(withAuthentication(Items))} />
         <Route path="/login" component={Login} />
-        <Route path="/loans" component={Loans} />
-    </Layout>);
+        <Route path="/loans" component={connect(mapStateToProps, null)(withAuthentication(Loans))} />
+        <Route path="/register" component={Register} />
+    </React.Fragment>);
