b0ba41e50dcec1085deafb05bffdd2e23bb942f5
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-28 13:12:08 +0300

    Print loans as a table

--- dotnet-mysql-application/ClientApp/pages/Loans/Loans.tsx
+++ dotnet-mysql-application/ClientApp/pages/Loans/Loans.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
 import { connect, Dispatch } from 'react-redux';
 import { ApplicationState } from '../../store';
-import { getLoans } from '../../store/loan/actions';
-import { LoansState, Loan } from '../../store/loan/types';
-import LoanListItem from './containers/LoanListItem';
+import { getLoans, returnLoan } from '../../store/loan/actions';
+import { LoansState, Loan, LoanReturnRequest } from '../../store/loan/types';
 import { Table, Grid, Row, Col } from 'react-bootstrap';
 
 interface LoansPageProps {
     getLoans: () => any;
+    returnLoan: (request: LoanReturnRequest) => any;
 }
 
 type AllProps = LoansPageProps & LoansState & typeof getLoans & RouteComponentProps<{}>;
@@ -17,12 +17,22 @@ class Loans extends React.Component<AllProps> {
     constructor(props: AllProps) {
         super(props);
         this.renderLoansList = this.renderLoansList.bind(this);
-        this.renderLoanRow = this.renderLoanRow.bind(this);
+        this.renderActiveLoanRow = this.renderActiveLoanRow.bind(this);
+        this.renderReturnedLoanRow = this.renderReturnedLoanRow.bind(this);
     }
 
     public componentWillMount(): void {
         console.log(this.props);
         this.props.getLoans();
+    }
+
+    onReturn = (event: any): void => {
+        const loanId = event.target.dataset.id;
+        const now = Date();
+        this.props.returnLoan({id: loanId, clientTime: now});
+        console.log(event.target);
+        console.log(loanId);
+        console.log(event.target.dataset.id);
     }
 
     public render() {
@@ -71,20 +81,22 @@ class Loans extends React.Component<AllProps> {
         );
     }
     private renderLoansList() {
+        const activeLoans = this.props.loans.filter(loan => loan.active === true);
         return (
             <>
-                {this.props.loans.map(loan => <tr key={loan.id}>{this.renderLoanRow(loan)}</tr>)}
+                {activeLoans.map(loan => <tr key={loan.id}>{this.renderActiveLoanRow(loan)}</tr>)}
             </>
         );
     }
     private renderReturnedLoansList() {
+        const returnedLoans = this.props.loans.filter(loan => loan.active === false);
         return (
             <>
-                {this.props.loans.map(loan => <tr key={loan.id}>{this.renderLoanRow(loan)}</tr>)}
+                {returnedLoans.map(loan => <tr key={loan.id}>{this.renderReturnedLoanRow(loan)}</tr>)}
             </>
         );
     }
-    private renderLoanRow(loan: Loan) {
+    private renderActiveLoanRow(loan: Loan) {
         return (
             <>
                 <td>{loan.item.name}</td>
@@ -92,7 +104,20 @@ class Loans extends React.Component<AllProps> {
                 {/* JavaScript Date-object parsing cannot be trusted cross-browser? */}
                 <td>{new Date(loan.startDate).toLocaleDateString('fi-FI')}</td>
                 <td>{new Date(loan.endDate).toLocaleDateString('fi-FI')}</td>
-                <td>Return button here</td>
+                <td><a onClick={this.onReturn} data-id={loan.id}>Return</a></td>
+            </>
+        );
+    }
+
+    private renderReturnedLoanRow(loan: Loan) {
+        return (
+            <>
+                <td>{loan.item.name}</td>
+                <td>{loan.user}</td>
+                {/* JavaScript Date-object parsing cannot be trusted cross-browser? */}
+                <td>{new Date(loan.startDate).toLocaleDateString('fi-FI')}</td>
+                <td>{new Date(loan.endDate).toLocaleDateString('fi-FI')}</td>
+                <td>{new Date(loan.returnDate).toLocaleDateString('fi-FI')}</td>
             </>
         );
     }
@@ -102,5 +127,6 @@ export default connect(
     (state: ApplicationState) => state.loans,
     (dispatch: Dispatch) => ({
         getLoans: () => dispatch(getLoans()),
+        returnLoan: (request: LoanReturnRequest) => dispatch(returnLoan(request)),
     })
 )(Loans);
