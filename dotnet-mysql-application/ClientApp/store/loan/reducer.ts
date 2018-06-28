1b0ff97b9d90d7245786e0a0902b7d0cc634d6a0
Tomi Heino <tomi.heino@eng.tamk.fi>
2018-06-28 13:10:12 +0300

    Support returning loans in reducer

--- dotnet-mysql-application/ClientApp/store/loan/reducer.ts
+++ dotnet-mysql-application/ClientApp/store/loan/reducer.ts
@@ -1,7 +1,7 @@
 import { Reducer } from 'redux';
-import { LoansState, GetLoansActions, AddLoanActions, Loan } from './types';
+import { LoansState, GetLoansActions, AddLoanActions, Loan, ReturnLoanActions } from './types';
 
-type AllLoanActions = GetLoansActions | AddLoanActions;
+type AllLoanActions = GetLoansActions | AddLoanActions | ReturnLoanActions;
 
 export const initialState: LoansState = {
   loans: [],
@@ -25,9 +25,26 @@ const loansReducer: Reducer<LoansState> = (state: LoansState = initialState, act
     case '@@loans/ADD_FAILURE':
       // Somehow communicate to UI that we fucked up
       return { ...state, isLoading: false };
+    case '@@loans/RETURN':
+      return { ...state, isLoading: true };
+    case '@@loans/RETURN':
+      return { ...state, isLoading: false, loans: updateObjectInArray(state.loans, action.payload.loan)};
+      // return { ...state, isLoading: false, loans: [...state.loans, action.payload.loan ] };
+    case '@@loans/RETURN_FAILURE':
+      // Somehow communicate to UI that we fucked up
+      return { ...state, isLoading: false };
     default:
       return state;
   }
+};
+
+// This would be a cool case to use generics, but all our types would have to extend a type that always has id..
+const updateObjectInArray = (array: Loan[], newLoan: Loan) => {
+  const index = array.findIndex(item => item.id === newLoan.id);
+  let newArray = array.slice(0, index);
+  newArray = newArray.concat(newLoan);
+  newArray = newArray.concat(array.slice(index + 1, array.length));
+  return newArray;
 };
 
 export default loansReducer;
